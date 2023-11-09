b005b6978a872db0ace1a6a760d8e3ca738a9208
VIndsudefe <otasmatov@gmail.com>
2023-11-09 09:52:20 +0600

    pochti

--- app.js
+++ app.js
@@ -5,3 +5,25 @@ onload = () => {
       clearTimeout(c);
     }, 1000);
   };
+
+let block1 = document.querySelector(".part_1")
+
+block1.style.display = "none"
+
+
+
+let block2 = document.querySelector(".part2")
+const music = document.getElementById('music');
+
+block2.onclick =()=>{
+  block1.style.display = "block"
+  block2.style.display = "none"
+  if (music.paused) {
+    console.log("rabotayet");
+    music.play();
+    
+} else {
+    
+    music.pause();
+}
+}
